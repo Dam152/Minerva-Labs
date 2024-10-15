55933c4aa62f6189fef54761a10efca4b55ac5e9
damjaguar98@icloud.com <damjaguar98@icloud.com>
2024-10-15 20:01:00 +0200

    add modify header

--- src/app/_componets/organisms/Header.jsx
+++ src/app/_componets/organisms/Header.jsx
@@ -1,12 +1,40 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import CtaButton from "../atoms/CtaButton";
 import Text from "../atoms/Text";
 import Link from "next/link";
+import { RiMenu3Fill } from "react-icons/ri";
+import { RiCloseLargeFill } from "react-icons/ri";
 
 export default function Header({ menu }) {
+  const [isOpen, setIsopen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsopen(false);
+      setIsClosing(false);
+    }, 1000);
+  };
+
+  const handleMenuClick = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsopen(false);
+      setIsClosing(false);
+    }, 1000);
+  };
+
+  if (isOpen == true) {
+    document.body.classList.add("no-scroll");
+  } else {
+    document.body.classList.remove("no-scroll");
+  }
+
   return (
-    <header className=" fixed w-screen border border-black py-[32px] px-[80px] max-xl:px-[32px] max-lg:h-[118px] max-md:px-[24px] max-sm:px-[16px] max-sm:h-[80px] ">
+    <header className=" fixed w-screen border-b border-[#DADADA] py-[32px] px-[80px] max-xl:px-[32px] max-md:px-[24px] max-sm:px-[16px] max-lg:py-[48px] max-sm:py-[40px]">
       <div className="flex items-center justify-center w-full h-full relative">
         <Link href="/" className="absolute left-0">
           <Image
@@ -14,7 +42,7 @@ export default function Header({ menu }) {
             alt="logo"
             width={168}
             height={41}
-            className="w-[168px] h-[41px] "
+            className="w-[168px] h-[41px]"
           ></Image>
         </Link>
         <nav className="flex gap-[40px] max-lg:hidden">
@@ -24,16 +52,48 @@ export default function Header({ menu }) {
               href={m.url}
               size="medium-text"
               weight="text-small"
-              className="hover:transition-all hover:duration-100 hover:opacity-60"
+              className="menu-voice"
             >
               {m.voice}
             </Text>
           ))}
         </nav>
-        <CtaButton href={""} className="absolute right-0 max-sm:hidden">
+        <CtaButton href={""} className="absolute right-0 max-lg:hidden">
           Contact
         </CtaButton>
+        <RiMenu3Fill
+          className="w-[32px] h-[32px] absolute right-0 hidden max-lg:block color-[var(--Black)] cursor-pointer"
+          onClick={() => setIsopen(!isOpen)}
+        ></RiMenu3Fill>
       </div>
+      {isOpen && (
+        <div
+          className={`w-screen h-screen flex flex-col absolute top-0 left-0 z-50 bg-[var(--Blue)] ${
+            isClosing ? "closeMenu" : "openMenu"
+          }`}
+        >
+          <div className="p-[32px] max-sm:py-[24px] max-sm:px-[16px]">
+            <RiCloseLargeFill
+              className="w-[32px] h-[32px] ml-auto hidden max-lg:block close-icon cursor-pointer"
+              onClick={handleClose}
+            ></RiCloseLargeFill>
+          </div>
+          <nav className="flex flex-col gap-[32px] w-full items-center mt-[48px] overflow-scroll">
+            {menu.map((m) => (
+              <Text
+                key={Math.random()}
+                href={m.url}
+                size="medium-text"
+                weight="text-medium"
+                theme="White"
+                onClick={handleMenuClick}
+              >
+                {m.voice}
+              </Text>
+            ))}
+          </nav>
+        </div>
+      )}
     </header>
   );
 }
--- src/app/assets/js/colorMap.js
+++ src/app/assets/js/colorMap.js
@@ -3,7 +3,7 @@ const colors = {
   Blue: "var(--Blue)",
   Body: "var(--Body)",
   Gray2: "var(--Gray-2)",
-  White: "var(--Color---White)",
+  White: "var(--Color-White)",
 };
 
 export default colors;
